04a4ed1157203511aa061555528cdbc3f10e8d17
AuGom <augusto.pg@live.com>
2020-03-21 00:44:29 -0300

    Updating

--- Challange 1/hello.go
+++ Challange 1/hello.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World."))
+
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+	r := mux.NewRouter()
+	r.HandleFunc("/", hello)
+	http.ListenAndServe(":8081", r)
 }
